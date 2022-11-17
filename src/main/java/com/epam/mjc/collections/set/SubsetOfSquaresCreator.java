1abe9c5c20978fa200dbce128abd617ddcacafa6
sergeikonash <38048905+sergeikonash@users.noreply.github.com>
2022-11-17 15:24:35 +0100

    Update SubsetOfSquaresCreator.java

--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -2,8 +2,20 @@ package com.epam.mjc.collections.set;
 
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        for (Integer integer : sourceList) {
+            if (powOfNumber(integer) >= lowerBound && powOfNumber(integer) <= upperBound) {
+                treeSet.add(powOfNumber(integer));
+            }
+        }
+        return treeSet;
+    }
+
+    private Integer powOfNumber(int number) {
+        return (int) Math.pow(number, 2);
     }
 }
